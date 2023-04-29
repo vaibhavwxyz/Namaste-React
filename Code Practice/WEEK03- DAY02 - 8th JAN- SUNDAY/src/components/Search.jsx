3a1ff310ebe2a7db44cc6c583460ba8de98494ca
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-04-29 22:11:32 +0530

    search

--- Code Practice/WEEK03- DAY02 - 8th JAN- SUNDAY/src/components/Search.jsx
+++ Code Practice/WEEK03- DAY02 - 8th JAN- SUNDAY/src/components/Search.jsx
@@ -1,4 +1,7 @@
 const Search = () => {
+  const handelSearch = async (e) => {
+    e.preventDefault();
+  };
   return (
     <div class="flex w-full max-w-sm items-center space-x-2 my-5">
       <input
@@ -6,7 +9,10 @@ const Search = () => {
         type="email"
         placeholder="what food are you craving today?"
       />
-      <button class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4">
+      <button
+        class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4"
+        onClick={handelSearch}
+      >
         Search
       </button>
     </div>
