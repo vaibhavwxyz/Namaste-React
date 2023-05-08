b49ab778ec7f75746e93eb805a4ed79ed8d10576
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-05-08 13:02:55 +0530

    search update

--- Code Practice/WEEK04- DAY02 - 15th JAN- SUNDAY/src/components/Search.jsx
+++ Code Practice/WEEK04- DAY02 - 15th JAN- SUNDAY/src/components/Search.jsx
@@ -4,8 +4,8 @@ const Search = ({ allRestaurant, setFilteredRestaurants }) => {
   const [search, setSearch] = useState();
 
   function handelSearch() {
-    const tempData = allRestaurant.filter(
-      (data) => data.strMeal.toLowerCase() === search.toLowerCase()
+    const tempData = allRestaurant.filter((data) =>
+      data.strMeal.toLowerCase().includes(search)
     );
     return tempData;
   }
@@ -21,7 +21,7 @@ const Search = ({ allRestaurant, setFilteredRestaurants }) => {
       <button
         className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4"
         onClick={() => {
-          const filteredData = handelSearch(search, allRestaurant);
+          const filteredData = handelSearch();
           setFilteredRestaurants(filteredData);
         }}
       >
