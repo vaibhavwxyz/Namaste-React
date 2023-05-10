129b508283a3c28fd360a798f21dec880a9599be
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-05-11 00:32:57 +0530

    login button fixed

--- Code Practice/WEEK05- DAY01 - 21st JAN- SATURDAY/src/components/Button.jsx
+++ Code Practice/WEEK05- DAY01 - 21st JAN- SATURDAY/src/components/Button.jsx
@@ -1,11 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function Button() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleButtonClick = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
   return (
-    <button className="text-[#ee8512] border-[#ee8512] border-[1.2px] rounded-full px-5 py-2 flex items-center justify-center font-medium sm:hover:bg-[#ee8512] sm:hover:text-white transition-all duration-200 ease-in-out">
+    <button
+      className="relative text-[#ee8512] border-[#ee8512] border-[1.2px] rounded-full px-5 py-2 flex items-center justify-center font-medium sm:hover:bg-[#ee8512] sm:hover:text-white transition-all duration-200 ease-in-out w-28"
+      onClick={handleButtonClick}
+    >
       <FontAwesomeIcon icon={faUser} className="mr-2" />
-      Login
+      {isLoggedIn ? "Logout" : "Login"}
     </button>
   );
 }
